733d746f9782702aed4432ead5ca8f60edc2b1ea
Manik507 <baradmanik@gmail.com>
2026-06-07 23:46:19 +0530

    Completed Switch Statement

---
+++ Main/src/main/java/com/mycompany/main/CheckVowel.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.main;
+import java.util.*;
+/**
+ *
+ * @author Admin
+ */
+public class CheckVowel {
+    public static void main(String args[]) {
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter an character");
+        char ch=sc.next().charAt(0);
+        switch(ch)
+        {
+            case 'a':
+            case 'e':
+            case 'i':
+            case 'o':
+            case 'u':
+            case 'A':
+            case 'E':
+            case 'I':
+            case 'O':
+            case 'U':
+                System.out.print("You entered an vowel");
+                break;
+            default:
+                System.out.print("You not entered an vowel");
+        }
+    }
+}
