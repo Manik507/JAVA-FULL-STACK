7d947363946bda22b5d9424d35b42303aeca39db
Manik507 <baradmanik@gmail.com>
2026-07-11 23:32:39 +0530

    Completed Basic Linked List Methods Implementation

--- JavaPractice/src/main/java/MyLinkedList.java
+++ JavaPractice/src/main/java/MyLinkedList.java
@@ -34,8 +34,84 @@ public class MyLinkedList {
         head=newNode;
     }
     
+    //Add Last
+    public void addLast(String data)
+    {
+        
+        Node newNode=new Node(data);
+        if(head==null)
+        {
+            head=newNode;
+            return;
+        }
+        Node temp=head;
+        while(temp.next!=null)
+        {
+            temp=temp.next;
+        }
+        temp.next=newNode;
+    }
+    
+    //Remove First
+    public void removeFirst()
+    {
+        if(head==null)
+        {
+            System.out.println("No nodes in the list present");
+            return;
+        }
+        head=head.next;
+    }
+    
+    //Remove Last
+    public void removeLast()
+    {
+        if(head==null)
+        {
+            System.out.println("No nodes present in the list");
+            return;
+        }
+        if(head.next==null)
+        {
+            head=null;
+            return;
+        }
+        Node temp=head;
+        while(temp.next.next!=null)
+        {
+            temp=temp.next;
+        }
+        temp.next=null;
+    }
+    
+    //Print List
+    public void printList()
+    {
+        if(head==null)
+        {
+            System.out.println("No nodes in the list");
+            return;
+        }
+        
+        Node curr=head;
+        while(curr!=null)
+        {
+            System.out.print(curr.data+"->");
+            curr=curr.next;
+        }
+        System.out.print("NULL");
+    }
     public static void main(String args[]) {
         MyLinkedList ll=new MyLinkedList();
         ll.addFirst("Manik");
+        ll.addLast("Barad");
+        ll.printList();
+        System.out.println();
+        ll.removeFirst();
+        ll.printList();
+        System.out.println();
+        ll.removeLast();
+        System.out.println();
+        ll.printList();
     }
 }
