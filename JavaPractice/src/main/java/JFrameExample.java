a5e8605e96d55dadda8c36e40a3b9cdba011a554
Manik507 <baradmanik@gmail.com>
2026-07-15 23:33:22 +0530

    Learned about Swing - JFrame and difference between AWT and Swing

---
+++ JavaPractice/src/main/java/JFrameExample.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Admin
+ */
+import javax.swing.*;
+public class JFrameExample {
+    public static void main(String args[]) {
+        JFrame frame=new JFrame("My First Frame"); //Frame title
+        frame.setSize(300,400); //Frame's height and width
+        frame.setLayout(null); //No layout applied
+        frame.setVisible(true); //Can be viewed
+    }
+}
