871c00245fe9f01d75920f666464306ec48586dd
Manik507 <baradmanik@gmail.com>
2026-07-16 20:47:30 +0530

    Learned about JButton and JLabel

--- JavaPractice/src/main/java/JFrameExample.java
+++ JavaPractice/src/main/java/JFrameExample.java
@@ -11,8 +11,18 @@ import javax.swing.*;
 public class JFrameExample {
     public static void main(String args[]) {
         JFrame frame=new JFrame("My First Frame"); //Frame title
-        frame.setSize(300,400); //Frame's height and width
+        frame.setSize(300,400); //Frame's width and height
         frame.setLayout(null); //No layout applied
+        
+        JButton b1=new JButton(new ImageIcon("C:\\Users\\Admin\\Pictures\\Screenshots\\hi.png"));
+        JLabel j1=new JLabel("Created a Button with image");
+        
+        b1.setBounds(100,100,200,200); //x,y,w,h
+        j1.setBounds(100,70,200,20);
+        
+        frame.add(b1);
+        frame.add(j1);
+        
         frame.setVisible(true); //Can be viewed
     }
 }
